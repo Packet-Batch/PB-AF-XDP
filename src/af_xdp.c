4e536611bc0448c3a323a41f4024062ad0af0754
gamemann <gamemann@gflclan.com>
2021-12-01 16:55:44 +0000

    Try changing flags.

--- src/af_xdp.c
+++ src/af_xdp.c
@@ -119,9 +119,9 @@ static struct xsk_socket_info *xsk_configure_socket(struct xsk_umem_info *umem, 
 
     xsk_info->umem = umem;
     xsk_cfg.tx_size = XSK_RING_PROD__DEFAULT_NUM_DESCS;\
-    xsk_cfg.libbpf_flags = XSK_LIBBPF_FLAGS__INHIBIT_PROG_LOAD;
+    xsk_cfg.libbpf_flags = 0;
     xsk_cfg.xdp_flags = flags;
-    xsk_cfg.bind_flags = XDP_COPY;
+    xsk_cfg.bind_flags = 0;
 
     ret = xsk_socket__create(&xsk_info->xsk, dev, queue_id, umem->umem, NULL, &xsk_info->tx, &xsk_cfg);
 
