135e741eb5fcf744df18ecd353ca4d5a201012fe
gamemann <gamemann@gflclan.com>
2021-12-01 19:03:49 +0000

    Add more debugging.

--- src/af_xdp.c
+++ src/af_xdp.c
@@ -16,8 +16,11 @@
 #include <sys/socket.h>
 #include <linux/if_link.h>
 #include <linux/if_ether.h>
+#include <linux/ip.h>
 #include <bpf.h>
 #include <xsk.h>
+
+#include <arpa/inet.h>
 
 #include "af_xdp.h"
 
@@ -168,7 +171,10 @@ int send_packet(int thread_id, void *pckt, __u32 length, __u8 verbose)
 
     struct ethhdr *eth = (struct ethhdr *)addr;
 
+    struct iphdr *iph = (struct iphdr *)eth + 1;
+
     printf("%hhx:%hhx:%hhx:%hhx:%hhx:%hhx => %hhx:%hhx:%hhx:%hhx:%hhx:%hhx.\n", eth->h_source[0], eth->h_source[1], eth->h_source[2], eth->h_source[3], eth->h_source[4], eth->h_source[5], eth->h_dest[0], eth->h_dest[1], eth->h_dest[2], eth->h_dest[3], eth->h_dest[4], eth->h_dest[5]);
+    printf("%u => %u.\n", ntohl(iph->saddr), ntohl(iph->daddr));
 
     __u32 tx_idx = 0;
 
